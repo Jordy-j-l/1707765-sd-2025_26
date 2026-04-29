5da6c0e18946ecf00607aa59f2b2847be3deb801
Jordy <jordy.l.nav@gmail.com>
2026-04-29 23:03:57 +0100

    Cliente a insirir  o endereço
    o Cliente já consegue insirir um endereço e já obtem uma resposta do servidor

--- cliente.py
+++ cliente.py
@@ -9,28 +9,70 @@ from xmlrpc.client import ServerProxy
 # ==================================================
 # URL do servidor RPC
 SERVER_URL = "http://localhost:8000"
-client = ServerProxy(SERVER_URL)
-response = client.ping()
+def config():
+    client = ServerProxy(SERVER_URL)
+    response = client.ping()
+    print("Resposta do servidor:", response)
+    return client
+    
 
-print("Resposta do servidor:", response)
-
-# ==================================================
-# PEDIR ENDEREÇO AO UTILIZADOR
-# ==================================================
-# O utilizador escreve o endereço Ethereum
 
 
 # ==================================================
-# CHAMADA RPC AO SERVIDOR
+# CHAMADA RPC AO SERVIDOR/# APRESENTAR RESULTADO
+
 # ==================================================
 # O cliente chama a função remota:
 # obterSaldo(endereco)
 
 
-# ==================================================
-# APRESENTAR RESULTADO
-# ==================================================
+    
+
 # Mostrar no ecrã:
 # - endereço consultado
 # - saldo recebido
 # - mensagem de erro, se existir
+
+def Show(endereco):
+    client=config()
+    try:
+        resposta = client.obterSaldo(endereco)
+
+        print()
+        print("############### RESULTADO ##################")
+        print(f"Conta: {endereco}")
+        print(f"Resposta: {resposta}")
+        print("############################################")
+    except Exception as erro:
+        print()
+        print("################ ERRO ######################")
+        print("Não foi possível contactar o servidor RPC.")
+        print(f"Detalhe: {erro}")
+        print("############################################")
+
+# ==================================================
+# PEDIR ENDEREÇO AO UTILIZADOR/MAIN
+# ==================================================
+# O utilizador escreve o endereço Ethereum
+def menu():
+    print()
+    print("############################################")
+    print("#           ETHEREUM ACCOUNT                #")
+    print("#                 /\\                       #")
+    print("#                /  \\                      #")
+    print("#               /____\\                     #")
+    print("#               \\    /                     #")
+    print("#                \\  /                      #")
+    print("#                 \\/                       #")
+    print("############################################")
+    print()
+def main():
+    
+    menu()
+    endereco=input("INSIRA A CONTA DESEJADA =>")
+    print("############################################")
+    print("A enviar pedido ao servidor RPC...")
+    print("############################################")
+    Show(endereco)
+if __name__ == "__main__":
+    main()
